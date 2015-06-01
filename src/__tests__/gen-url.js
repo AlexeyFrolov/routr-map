1f2a9918db99a2aa870dee9bcb25d39e6e73a19c
fro <frol.rage@gmail.com>
2015-06-01 16:42:09 +0300

    test

--- src/__tests__/gen-url.js
+++ src/__tests__/gen-url.js
@@ -15,5 +15,8 @@ describe('Url generation', () => {
     it('should throw an exception if route requires params but no correct params provided', () => {
         expect(() => router.url({name: 'users.comments', params: {idd: 1}}))
             .toThrow(new Error("Parameter 'id' should be provided"));
+
+        //expect(() => router.url({name: 'users.commentss', params: {idd: 1}}))
+        //    .toThrow(new Error("Route with name '" + name.join(".") + "' does not exists"));
     });
 });
